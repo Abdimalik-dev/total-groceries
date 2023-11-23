e6cdbe63b12f8f547b9311e263a7f92f9da912c2
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-23 21:59:40 +0300

    completed

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Good Luck 💪🏾
+function getTotalPrice(Items) {
+
+    let totalPrice = 0
+    for (let i = 0; i < Items.length; i++) {
+        totalPrice = totalPrice + Items[i].price * Items[i].quantity
+    }
+    console.log(totalPrice)
+}
+
+
+getTotalPrice([
+        { product: "Milk", quantity: 1, price: 1.50 }
+    ])
+
+getTotalPrice([
+        { product: "Milk", quantity: 1, price: 1.50 },
+        { product: "Cereals", quantity: 1, price: 2.50 }
+    ])
+
+
+getTotalPrice([
+        { product: "Milk", quantity: 3, price: 1.50 }
+    ])
+  
